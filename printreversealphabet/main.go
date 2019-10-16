72e8a63fbcb3db76fe24f0b80f8032854011ff7a
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-16 17:25:33 +0600

    commitreverse

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func main() {
     for i := 'z'; i >= 'a'; i-- {
-        z01.PrintRune(i)
+            z01.PrintRune(i)
 
-    }
+}
     
         z01.PrintRune(10)
 
