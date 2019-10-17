a918099b3c0bf158ceb178723585426dbfd5eb77
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-17 19:58:52 +0600

    Printstr

--- printstr.go
+++ printstr.go
@@ -3,7 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	charOneByOne := []byte(str)
+	charOneByOne := []rune(str)
 	for _, letter := range charOneByOne {
 		z01.PrintRune("%v", letter)
 	}
