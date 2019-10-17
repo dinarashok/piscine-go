5fc85cfc1d8e60b5b8be7175e8efcd5a0dbedc52
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-17 19:53:28 +0600

    Printstr

--- printstr.go
+++ printstr.go
@@ -5,6 +5,6 @@ import "github.com/01-edu/z01"
 func PrintStr(str string) {
 	charOneByOne := []byte(str)
 	for _, letter := range charOneByOne {
-		z01.PrintRune(, letter)
+		z01.PrintRune("%v", letter)
 	}
 }
