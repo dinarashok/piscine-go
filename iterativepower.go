2247eb3de389a043ac3a2f18b1a5a36a77f3eea1
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-21 21:10:34 +0600

    commit

--- iterativepower.go
+++ iterativepower.go
@@ -3,7 +3,7 @@ package piscine
 func IterativePower(nb int, power int) int {
 	x := 1
 	if power > 0 {
-		for a := 1; a < power+1; a++ {
+		for a := 1; a < (power + 1); a++ {
 			x = x * nb
 		}
 	}
