5d9a76f96a90c0727598fdc75cb1ca0afe4826b5
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-21 22:58:57 +0600

    commit

--- sqrt.go
+++ sqrt.go
@@ -1,12 +1,11 @@
 package piscine
 
 func Sqrt(nb int) int {
-	if nb%2 == 0 {
-		x := 1
-		nb := nb / 2
-		x += 1
-		return x
-	} else {
-		return 0
+	var sqrt int
+	for a := 1; a <= nb; a++ {
+		if a*a == nb {
+			sqrt = i
+		}
 	}
+	return root
 }
