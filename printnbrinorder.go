46cbec91e3e866eb6228e48c865a3408357e2913
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-22 19:03:09 +0600

    message

---
+++ printnbrinorder.go
@@ -0,0 +1,29 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	var DivisionLeft []int
+
+	if n == 0 {
+		DivisionLeft = append(DivisionLeft, n)
+	}
+	indexNb := 0
+	for n > 0 {
+		DivisionLeft = append(DivisionLeft, n%10)
+		n = n / 10
+		indexNb++
+	}
+	for i := 0; i < indexNb; i++ {
+		for j := i + 1; j < indexNb; j++ {
+			if DivisionLeft[i] > DivisionLeft[j] {
+				t := DivisionLeft[i]
+				DivisionLeft[i] = DivisionLeft[j]
+				DivisionLeft[j] = t
+			}
+		}
+	}
+	for i := range k {
+		z01.PrintRune(rune(48 + k[i]))
+	}
+}
