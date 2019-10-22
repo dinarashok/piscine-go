41163d513f9ce57a988e6d76bc2ec417a5d689a0
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-22 14:01:11 +0600

    commit

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,10 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func AlphaCount(str string) int {
 	StrInRunes := []rune(str)
 	count := 0
 	for i := range StrInRunes {
-		if StrInRunes[i] >= 65 && StrInrRunes[i] <= 132 || StrInRunes[i] >= 97 && StrInrRunes[i] <= 122 {
+		if StrInRunes[i] >= 65 && StrInRunes[i] <= 132 || StrInRunes[i] >= 97 && StrInRunes[i] <= 122 {
 			count++
 		}
 	}
