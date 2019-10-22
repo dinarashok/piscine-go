de5769383f8a3df86cd42a65feddbe71e0a2be6e
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-22 14:02:27 +0600

    commit

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(str string) int {
 	StrInRunes := []rune(str)
 	count := 0
 	for i := range StrInRunes {
-		if StrInRunes[i] >= 65 && StrInRunes[i] <= 132 || StrInRunes[i] >= 97 && StrInRunes[i] <= 122 {
+		if StrInRunes[i] >= 65 && StrInRunes[i] <= 90 || StrInRunes[i] >= 97 && StrInRunes[i] <= 122 {
 			count++
 		}
 	}
