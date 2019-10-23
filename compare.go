a6d7c775ff600d9795a2f5e1382582e0538fdb37
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-23 13:04:58 +0600

    Compare returns an integer comparing two strings lexicographically. The result will be 0 if a==b, -1 if a < b, and +1 if a > b

---
+++ compare.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Compare(a, b string) int {
+	RuneA := []rune(a)
+	RuneB := []rune(b)
+
+	// find lenths of those massives
+	lenA := 0
+	lenB := 0
+	for range RuneA {
+		lenA++
+	}
+	for range RuneB {
+		lenB++
+	}
+
+	for i = 0; i < lenA-lenB; i++ {
+		if a == b {
+			return 0
+		}
+		if a > b {
+			return 1
+		}
+		return -1
+	}
+}
