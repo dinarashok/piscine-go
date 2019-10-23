de7b269d10407ed9d8130fa854241eabe76b5bfb
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-23 17:14:18 +0600

    true if the string passed in parameter only contains numerical characters

---
+++ isnumeric.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsNumeric(str string) bool {
+	RuneStr := []rune(str)
+	y := 0
+	for range str {
+		y++
+	}
+
+	for i := 0; i < y; i++ {
+		if RuneStr[i] >= '0' && RuneStr[i] <= '9' {
+			return true
+		}
+	}
+	return false
+}
