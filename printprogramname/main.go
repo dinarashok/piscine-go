0074eac8bb53c30ab063fa08099a9861051fafd7
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-23 20:22:27 +0600

    using os.Args print file name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -10,8 +10,8 @@ func main() {
 
 	AnyNameArg := []rune(os.Args[i])
 	// []rune(os.Args[0]) - massive of strings
-	// for _, i := range AnyNameArg {
-	z01.PrintRune(rune(AnyNameArg[0]))
-	// }
+	for _, i := range AnyNameArg {
+		z01.PrintRune(rune(AnyNameArg[0]))
+	}
 	z01.PrintRune('\n')
 }
