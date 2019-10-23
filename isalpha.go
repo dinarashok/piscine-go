a7a6edf6ad387d87a0f268ab0e48583d31358391
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-23 17:04:52 +0600

    true if the string contains only alphanumerical characters

---
+++ isalpha.go
@@ -0,0 +1,17 @@
+package piscine
+
+func IsAlpha(str string) bool {
+	RuneStr := []rune(str)
+	y := 0
+	for range RuneStr {
+		y++
+	}
+	for i := 0; i < y; i++ {
+		if RuneStr[i] < '0' || RuneStr[i] > '9' && RuneStr < 'A' || RuneStr[i] > 'Z' && RuneStr[i] < 'a' || RuneStr[i] > 'z' {
+			return false
+
+		}
+	}
+	return true
+
+}
