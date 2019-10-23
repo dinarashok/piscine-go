cffb5d34ae33c95925c40843f03764f05131ac9e
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-23 15:48:53 +0600

    capitalizes the first letter of each word and lowercases the rest. Alphanumerical characters

---
+++ capitalize.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Capitalize(s string) string {
+	lenS := 0
+	RuneS := []rune(s)
+	for range RuneS {
+		lenS++
+	}
+	for i := 0; i < lenS; i++ {
+		if RuneS[i] >= 'A' && RuneS[i] <= 'Z' {
+			RuneS[i] = (RuneS[i] + 32)
+		}
+		if RuneS[0] >= 'a' && RuneS[0] <= 'z' {
+			RuneS[0] = (RuneS[0] - 32)
+		}
+		if i > 0 {
+			if (RuneS[i-1] > 'Z' && RuneS[i-1] < 'a') || (RuneS[i-1] < 'A' && RuneS[i-1] > '9') || new[i-1] < '0' || new[i-1] > 'z' {
+				if RuneS[i] >= 'a' && RuneS[i] <= 'z' {
+					RuneS[i] = (RuneS[i] - 32)
+				}
+			}
+		}
+	}
+	answer := string(RuneS)
+	return answer
+}
