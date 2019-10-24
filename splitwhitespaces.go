e40c84e4bcd85aa08b1844b20e5095e8f29946a7
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 18:22:12 +0600

    commit

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,23 +1,21 @@
 package piscine
 
 func SplitWhiteSpaces(str string) []string {
-	rs := []rune(str)
-	lastSep := -1
-	count := 1
-	for i := range rs {
-		if rs[i] == '\t' || rs[i] == '\n' || rs[i] == ' ' {
-			count++
+	var MassiveStr []string
+	var b rune
+	c := 0
+	for i := range str {
+		if i == i {
+			c++
 		}
 	}
-	s := make([]string, count)
-	curr := 0
-	for i := range rs {
-		if rs[i] == '\t' || rs[i] == '\n' || rs[i] == ' ' {
-			s[curr] = string(rs[lastSep+1 : i])
-			curr++
-			lastSep = i
+	for i, char := range str {
+		for str[i] != ' ' {
+			b = b + char
 		}
+		d := string(b)
+		MassiveStr = append(MassiveStr, d)
 	}
-	s[count-1] = string(rs[lastSep+1 : my_rune_len(rs)])
-	return s
+
+	return MassiveStr
 }
