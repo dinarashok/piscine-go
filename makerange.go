cbff978e12934ff613db257f9a49ed63d52c5b53
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 16:07:37 +0600

    use make

--- makerange.go
+++ makerange.go
@@ -6,12 +6,13 @@ func MakeRange(min, max int) []int {
 	var MakeSlice []int
 
 	if min < max {
-		MakeSlice := make([]int, size)
+		MakeSlice = make([]int, size)
 		for i := min; i < max; i++ {
 			MakeSlice[i] = min + 1
 			return MakeSlice
 		}
 	} else {
+		var MakeSlice []int
 		return MakeSlice
 	}
 	return MakeSlice
