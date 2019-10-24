058bed7263b157dae7e739b7dcccae51fbcd5154
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 15:56:22 +0600

    use make

--- makerange.go
+++ makerange.go
@@ -3,8 +3,10 @@ package piscine
 func MakeRange(min, max int) []int {
 	var size int
 	size = max - min
+	var MakeSlice []int
+
 	if min < max {
-		MakeSlice := make([]int, size)
+		MakeSlice = make([]int, size)
 		for i := min; i < max; i++ {
 			MakeSlice[i] = min + 1
 		}
