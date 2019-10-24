df8797252b6234f0c6cb27dcbc0e0c6264148f80
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 12:21:43 +0600

    learn to use OS library

---
+++ printparams/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+
+	// call library for list of strings Args
+	ListArgs := os.Args
+
+	//find lenth
+	len := 0
+	for range ListArgs {
+		len++
+	}
+	// precise that need just parameters
+	for i := 1; i <= len; i++ {
+		// for range to get value
+		for i, value := range ListArgs {
+			z01.PrintRune(value)
+			z01.PrintRune('\n')
+		}
+	}
+}
