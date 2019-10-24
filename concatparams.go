8ee6d2da89c0badaf02070207d8a6981e406462e
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 16:16:11 +0600

    commit

--- concatparams.go
+++ concatparams.go
@@ -8,7 +8,7 @@ func ConcatParams(args []string) string {
 
 	var answer string
 	for i := range args {
-		if i != lenargs-1 {
+		if i != len-1 {
 			answer = answer + args[i] + "\n"
 		} else {
 			answer = answer + args[i]
