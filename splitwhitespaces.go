5631ebd5885519462c216c33abb427dc3922fa8b
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 18:19:30 +0600

    commit

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,9 +1,23 @@
 package piscine
 
-//SplitWhiteSpaces separates the words of a string and puts them in a string array.
-
 func SplitWhiteSpaces(str string) []string {
-	var MassiveStr []string
-	MassiveStr = append(MassiveStr, str)
-	return MassiveStr
+	rs := []rune(str)
+	lastSep := -1
+	count := 1
+	for i := range rs {
+		if rs[i] == '\t' || rs[i] == '\n' || rs[i] == ' ' {
+			count++
+		}
+	}
+	s := make([]string, count)
+	curr := 0
+	for i := range rs {
+		if rs[i] == '\t' || rs[i] == '\n' || rs[i] == ' ' {
+			s[curr] = string(rs[lastSep+1 : i])
+			curr++
+			lastSep = i
+		}
+	}
+	s[count-1] = string(rs[lastSep+1 : my_rune_len(rs)])
+	return s
 }
