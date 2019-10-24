72dc2990a8a4f9115906731c4d9d0aa7fb468fd4
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 13:35:01 +0600

    print arguments in reverse

---
+++ revparams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	ListArgs := os.Args
+
+	len := 0
+	for range ListArgs {
+		len++
+	}
+
+	for i := len - 1; i > 0; i++ {
+		for _, value := range ListArgs {
+			z01.PrintRune(value)
+		}
+	}
+}
