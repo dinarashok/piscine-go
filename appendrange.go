c0c100815b7160284bb02b327c0400fd38c0fb60
Dinara Shokabayeva <dinara.shokabay@gmail.com>
2019-10-24 15:10:45 +0600

    commit

---
+++ appendrange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	var SliceResult []int
+	if min < max {
+		for i := min; i < max; i++ {
+
+			SliceResult := append(SliceResult, i)
+		}
+	} else {
+		return SliceResult
+	}
+	return SliceResult
+}
